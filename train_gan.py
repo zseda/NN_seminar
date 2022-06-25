67c833fdab446f76a59986b6410ed5c2a57c6c6d
zeynepseda <zsedabirinci@gmail.com>
2022-06-25 21:48:23 +0200

    print predictions

--- train_gan.py
+++ train_gan.py
@@ -6,12 +6,29 @@ from torch.autograd import Variable
 from loguru import logger
 
 from data_loader import get_dataloader
+from matplotlib import pyplot
 from model import Discriminator, Generator
+
+# plot the generated images
+
+
+def create_plot(examples, n):
+    # plot images
+    for i in range(n * n):
+        # define subplot
+        pyplot.subplot(n, n, 1 + i)
+        # turn off axis
+        pyplot.axis('off')
+        # plot raw pixel data
+        image = examples[i, :, :].detach().numpy()
+        image = image[0]/255
+        pyplot.imshow(image)
+    pyplot.show()
 
 
 def main(
     epochs: int = typer.Option(20),
-    batch_size: int = typer.Option(16),
+    batch_size: int = typer.Option(100),
     lr: float = typer.Option(0.0002),
     z_dim: int = typer.Option(100)
 
@@ -52,7 +69,7 @@ def main(
                 torch.zeros(batch_size, 1).to(device))
 
             # discriminator
-            D_out = D(x_real)
+            D_out = D(img)
             D_real_loss = criterion(D_out, y_real)
             D_out = D(x_fake)
             D_fake_loss = criterion(D_out, y_fake)
@@ -72,9 +89,13 @@ def main(
             G_loss.backward()
             G_optimizer.step()
             G_losses.append(G_loss.data.item())
+        print('[%d/%d]: loss_d: %.3f, loss_g: %.3f' % (
+            (e), epochs, torch.mean(torch.FloatTensor(D_losses)), torch.mean(torch.FloatTensor(G_losses))))
 
-            print('[%d/%d]: loss_d: %.3f, loss_g: %.3f' % (
-                (e), epochs, torch.mean(torch.FloatTensor(D_losses)), torch.mean(torch.FloatTensor(G_losses))))
+        # scale from [-1,1] to [0,1]
+        #G_output = (G_output + 1) / 2.0
+        # plot the result
+        #create_plot(G_output, 10)
 
 
 if __name__ == "__main__":
