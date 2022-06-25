10eba395956722806abf53d29bb7bb932b3cd177
zeynepseda <zsedabirinci@gmail.com>
2022-06-25 22:14:51 +0200

    tensorboard print added

--- train_gan.py
+++ train_gan.py
@@ -4,37 +4,29 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from loguru import logger
-
+from pathlib import Path
+import uuid
 from data_loader import get_dataloader
 from matplotlib import pyplot
 from model import Discriminator, Generator
-
-# plot the generated images
-
-
-def create_plot(examples, n):
-    # plot images
-    for i in range(n * n):
-        # define subplot
-        pyplot.subplot(n, n, 1 + i)
-        # turn off axis
-        pyplot.axis('off')
-        # plot raw pixel data
-        image = examples[i, :, :].detach().numpy()
-        image = image[0]/255
-        pyplot.imshow(image)
-    pyplot.show()
+from torchvision.utils import make_grid
+from torch.utils.tensorboard import SummaryWriter
 
 
 def main(
+    root_path: str = typer.Option('.'),
     epochs: int = typer.Option(20),
     batch_size: int = typer.Option(100),
     lr: float = typer.Option(0.0002),
-    z_dim: int = typer.Option(100)
+    z_dim: int = typer.Option(100),
+    experiment_id: str = typer.Option(f"debug-{uuid.uuid4()}"),
 
 ):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logger.info(f"batch_size: {batch_size}")
+    tb_path = Path(root_path, "logs", experiment_id)
+    tb_path.mkdir(parents=True, exist_ok=False)
+    tb_writer = SummaryWriter(log_dir=tb_path.as_posix())
 
     # load data
     loader_train, loader_test, mnist_dim = get_dataloader(
@@ -54,7 +46,7 @@ def main(
 
     # loss
     criterion = nn.BCELoss()
-
+    global_step = 0
     D_losses, G_losses = [], []
     for e in range(1, epochs+1):
         logger.info(f"training epoch {e}/{epochs}")
@@ -62,6 +54,7 @@ def main(
             img, class_idx = batch
             img = img.to(device)
             class_idx = class_idx.to(device)
+            global_step += 1
             x_real, y_real = Variable(
                 img.view(-1, mnist_dim).to(device)), Variable(torch.ones(batch_size, 1).to(device))
             z = Variable(torch.randn(batch_size, z_dim).to(device))
@@ -78,6 +71,10 @@ def main(
             D_loss = D_real_loss + D_fake_loss
             D_loss.backward()
             D_optimizer.step()
+            if global_step % 50 == 0:
+                tb_writer.add_scalar(
+                    'train/loss', D_loss.item(), global_step=global_step)
+                tb_writer.flush()
             D_losses.append(D_loss.data.item())
 
             # generate images via G
@@ -88,14 +85,22 @@ def main(
             # gradient backprop & optimize ONLY G's parameters
             G_loss.backward()
             G_optimizer.step()
+            if global_step % 50 == 0:
+                tb_writer.add_scalar(
+                    'train/loss', G_loss.item(), global_step=global_step)
+                tb_writer.flush()
             G_losses.append(G_loss.data.item())
-        print('[%d/%d]: loss_d: %.3f, loss_g: %.3f' % (
-            (e), epochs, torch.mean(torch.FloatTensor(D_losses)), torch.mean(torch.FloatTensor(G_losses))))
 
-        # scale from [-1,1] to [0,1]
-        #G_output = (G_output + 1) / 2.0
-        # plot the result
-        #create_plot(G_output, 10)
+            if global_step % 100 == 0:
+                tb_writer.add_image(
+                    f"train/img", make_grid(img), global_step=global_step)
+                tb_writer.add_image(
+                    f"train/pred", make_grid(G_output), global_step=global_step)
+
+            # save network
+            if e % 1 == 0:
+                torch.save(D.state_dict(), Path(root_path, "logs",
+                           experiment_id, f"model_epoch_{e:0>3}.pth").as_posix())
 
 
 if __name__ == "__main__":
